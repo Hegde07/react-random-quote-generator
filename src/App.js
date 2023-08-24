8bdae477b218f2e3b20d0b0ca6a075d0a8b9f58c
Hegde07 <hegdebhargav92@gmail.com>
2023-08-24 12:54:39 -0700

    Completed React-random-quote-generator-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+//using type.fit quotes api
+import "./App.css";
+import { useEffect, useState, useRef } from "react";
 function App() {
+  const [quotes, setQuotes] = useState("");
+  const textRef = useRef();
+  let colors = ["#ffff00", "#90ee90", "#ffa500", "#ff68ff", "#a9a9e7"];
+
+  const getQuotes = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomNum]);
+      });
+  };
+  useEffect(() => {
+    getQuotes();
+  }, []);
+  useEffect(() => {
+    textRef.current.style.color =
+      colors[Math.floor(Math.random() * colors.length)];
+  }, [quotes]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote">
+        <p ref={textRef}>"{quotes.text}"</p>
+        <p>Author : {quotes.author}</p>
+        <div className="btnContainer">
+          <button className="btn" onClick={getQuotes}>
+            Get Quotes
+          </button>
+          <a
+            href={`https://twitter.com/intent/tweet?text=${quotes.text}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn"
+          >
+            tweet
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
